5f2d1779917e8f1c5126f583dfdc7bd8ccf974cc
Thibault Walterspieler <thibaultwalterspieler@gmail.com>
2020-01-18 14:08:33 +0100

    feat : add custom email form

--- page-mouvement.php
+++ page-mouvement.php
@@ -18,20 +18,25 @@ get_header();
     <div class="quotes-author">Antoine de Saint-Exupéry</div>
 </div>
 
-<div class="mouvement-text-wrapper padding-top-bottom-5">
-    <div class="title-2 white-color text-center margin-bottom-3">
+<div class="mouvement-text-wrapper ptb-5">
+    <div class="title-2 white-color text-center mb-3">
         Paris Avenir, pour un futur radieux
     </div>
     <p class="paragraph-1 text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque culpa dolorem fuga ab tempora exercitationem omnis beatae quas, dignissimos sit hic necessitatibus impedit optio asperiores cumque. Delectus laboriosam commodi harum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae perferendis earum, dolore excepturi, unde similique neque magnam voluptatum aliquid officiis eligendi soluta quos maiores. Ea unde cumque delectus quod laborum!</p>
 </div>
 
-<div class=" membership-wrapper text-center padding-top-bottom-5">
-    <div class="title-2 white-color margin-bottom-3echo " # wp_voteforme_theme">> README.md">
+<div class="membership-wrapper text-center ptb-5">
+    <div class="title-2 white-color mb-3">
         Nous avons besoin de vous !
     </div>
     <p class="paragraph-1 text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum optio odio doloremque aliquid. Quisquam iste illo quaerat perspiciatis nam ducimus nisi magnam voluptate, blanditiis a animi. Nesciunt rem esse blanditiis.</p>
-    <div class="more-membership-button">
-        S'engager
+    <div class="form-container">
+        <div class="group">
+            <input type="text" required>
+            <span class="highlight"></span>
+            <span class="bar"></span>
+            <label>Adresse email</label>
+        </div>
     </div>
 </div>
 
